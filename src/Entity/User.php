26bef5ff9880e6fb0bc9216ce1d0a1c5f4aa178e
Alexandre LOUBEAU <loubeau.alexandre3@gmail.com>
2024-04-23 12:12:16 +0200

    Creating Entity

---
+++ src/Entity/User.php
@@ -0,0 +1,302 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $email = null;
+
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 15)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 15)]
+    private ?string $lastname = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $updated_at = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $lastNicknameUsed = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $last_login = null;
+
+    #[ORM\Column]
+    private ?int $passwordResetCounter = null;
+
+    #[ORM\Column]
+    private ?bool $PasswordRequested = null;
+
+    #[ORM\Column(length: 15, nullable: true)]
+    private ?string $phone = null;
+
+    /**
+     * @var Collection<int, PlayerTeam>
+     */
+    #[ORM\OneToMany(targetEntity: PlayerTeam::class, mappedBy: 'id_player')]
+    private Collection $playerTeams;
+
+    public function __construct()
+    {
+        $this->playerTeams = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     *
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updated_at): static
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    public function getLastNicknameUsed(): ?string
+    {
+        return $this->lastNicknameUsed;
+    }
+
+    public function setLastNicknameUsed(?string $lastNicknameUsed): static
+    {
+        $this->lastNicknameUsed = $lastNicknameUsed;
+
+        return $this;
+    }
+
+    public function getLastLogin(): ?\DateTimeInterface
+    {
+        return $this->last_login;
+    }
+
+    public function setLastLogin(\DateTimeInterface $last_login): static
+    {
+        $this->last_login = $last_login;
+
+        return $this;
+    }
+
+    public function getPasswordResetCounter(): ?int
+    {
+        return $this->passwordResetCounter;
+    }
+
+    public function setPasswordResetCounter(int $passwordResetCounter): static
+    {
+        $this->passwordResetCounter = $passwordResetCounter;
+
+        return $this;
+    }
+
+    public function isPasswordRequested(): ?bool
+    {
+        return $this->PasswordRequested;
+    }
+
+    public function setPasswordRequested(bool $PasswordRequested): static
+    {
+        $this->PasswordRequested = $PasswordRequested;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): static
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, PlayerTeam>
+     */
+    public function getPlayerTeams(): Collection
+    {
+        return $this->playerTeams;
+    }
+
+    public function addPlayerTeam(PlayerTeam $playerTeam): static
+    {
+        if (!$this->playerTeams->contains($playerTeam)) {
+            $this->playerTeams->add($playerTeam);
+            $playerTeam->setIdPlayer($this);
+        }
+
+        return $this;
+    }
+
+    public function removePlayerTeam(PlayerTeam $playerTeam): static
+    {
+        if ($this->playerTeams->removeElement($playerTeam)) {
+            // set the owning side to null (unless already changed)
+            if ($playerTeam->getIdPlayer() === $this) {
+                $playerTeam->setIdPlayer(null);
+            }
+        }
+
+        return $this;
+    }
+}
