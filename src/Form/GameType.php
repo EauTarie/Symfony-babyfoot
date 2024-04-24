f41cab47d5172ccee39ae214f8888131087e5f3d
Alexandre <106594791+EauTarie@users.noreply.github.com>
2024-04-24 12:27:44 +0200

    create match controller

---
+++ src/Form/GameType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Game;
+use App\Entity\Team;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GameType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('created_at', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updated_at', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('occuring_at', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('duration')
+            ->add('score')
+            ->add('winningReason')
+            ->add('id_team', EntityType::class, [
+                'class' => Team::class,
+                'choice_label' => 'id',
+            ])
+            ->add('secondTeam', EntityType::class, [
+                'class' => Team::class,
+                'choice_label' => 'id',
+            ])
+            ->add('winner', EntityType::class, [
+                'class' => Team::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Game::class,
+        ]);
+    }
+}
