288d7590014fdf5c4a32a0cf51d8f2c68b1054e8
EauTarie <loubeau.alexandre3@gmail.com>
2024-05-24 09:53:37 +0200

    creating datafixtures

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(GameRepository $gameRepository): Response
     {
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'game' => $gameRepository->findBy(array(), array('id' => 'DESC'), 10, 0),
+            'lastGame' => $gameRepository->findOneBy(array(), array('id' => 'DESC')),
         ]);
     }
 }
